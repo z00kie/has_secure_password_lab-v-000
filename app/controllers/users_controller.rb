3ce62e9d5a4f2f10bf7e8b9e5fb5270ad23123c5
Emmy White <emmalinerosewhite@gmail.com>
2020-03-05 17:15:20 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+   def create
+        if params[:user][:password] == params[:user][:password_confirmation]
+            @user = User.new(user_params)
+            if @user.save
+                session[:user_id] = @user.id
+                redirect_to @user
+            else
+                redirect_to 'signup', alert: "Couldn't signup"
+            end
+        else
+            redirect_to new_user_path, alert: "Couldn't signup"
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:name, :password)
+    end
 end
