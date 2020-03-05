0daff34d126529d0e03c82bef128e2ed9c518569
Emmy White <emmalinerosewhite@gmail.com>
2020-03-05 17:14:56 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,10 @@
 class SessionsController < ApplicationController
+   def create
+        @user = User.find_by(name: params[:user][:name])
+        if @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+        else
+            redirect_to '/login', alert: "Couldn't log in"
+        end
+    end
 end
